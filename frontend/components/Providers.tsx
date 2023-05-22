f97f408ab64d70a44150fe0863070951575fe5a6
Adi-K527 <adikandakurtikar2@gmail.com>
2023-05-21 22:51:50 -0500

    t

--- frontend/components/Providers.tsx
+++ frontend/components/Providers.tsx
@@ -3,7 +3,7 @@ import { ApolloClient, InMemoryCache, ApolloProvider } from "@apollo/client";
 
 export const Providers = ({ children }: { children: React.ReactNode }) => {
 	const client = new ApolloClient({
-		uri: "http://localhost:5000/graphql",
+		uri: "https://takenoteapi.onrender.com/:5000/graphql",
 		cache: new InMemoryCache(),
 	});
 	return <ApolloProvider client={client}>{children}</ApolloProvider>;
