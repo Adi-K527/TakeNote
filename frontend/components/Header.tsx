ca6045a70622c3875558b096c9b254fb70fbce69
Adi-K527 <adikandakurtikar2@gmail.com>
2023-05-24 22:10:23 -0500

    /

--- frontend/components/Header.tsx
+++ frontend/components/Header.tsx
@@ -1,7 +1,7 @@
 import { useSession } from 'next-auth/react'
 import React from 'react'
 import {signOut} from "next-auth/react"
-
+//
 
 export default function Header() {
   const session = useSession()
